284fd1cd7ce7ece75cf040b216e81e8ffa03f29e
codseo <150895168+codseo@users.noreply.github.com>
2023-11-23 13:15:33 +0330

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,12 @@
+// Example SEO functions
+function check_page_speed() {
+    // Your code to check page speed
+}
+
+function check_links() {
+    // Your code to check links
+}
+
+function seo_page_info() {
+    // Your code to display SEO information
+}
