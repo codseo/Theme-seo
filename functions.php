13f7d5888c670dcb8143a33a5f71e6984503d98a
codseo <150895168+codseo@users.noreply.github.com>
2023-11-23 13:17:26 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,16 @@ function check_links() {
 function seo_page_info() {
     // Your code to display SEO information
 }
+// در فایل functions.php
+function add_google_analytics() {
+    ?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_ANALYTICS_ID"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'YOUR_ANALYTICS_ID');
+    </script>
+    <?php
+}
+add_action('wp_head', 'add_google_analytics');
