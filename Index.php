fe4c580b52c9bf54cbd8b7dfb4193d7005dc8169
codseo <150895168+codseo@users.noreply.github.com>
2023-11-23 13:13:19 +0330

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,16 @@
-<?php
-// Silence is golden.
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <h1><?php echo bloginfo('name'); ?></h1>
+        <p><?php echo bloginfo('description'); ?></p>
+    </div>
+</header>
